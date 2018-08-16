bf4d4368d8e1895d551d2339329ab29df3c603c3
Dante De Ruwe <dantederuwe@gmail.com>
2018-08-16 12:40:51 +0200

    // WIP
    Cleaned up the code

--- content.js
+++ content.js
@@ -1,23 +1,16 @@
-function getElementByXpath(path) {
-  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-}
-
-
-console.log('test');
+function getElementByXpath(path) {return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;}
 
 // Get the title and artist
-var x = JSON.parse(getElementByXpath('/html/body/script[5]').innerHTML);
+let x = JSON.parse(getElementByXpath('/html/body/script[5]').innerHTML);
 
 title = x.name;
 artist = (x.byArtist.name).includes('Misc') ? '' : x.byArtist.name;
 
-// Get the YT link
+// Make the YT link
 let url = `https://www.youtube.com/results?search_query=${(title + '+' + artist).replace(" ", "+")}`;
-
 
 // Look for top result
 let xhr = new XMLHttpRequest();
-
 xhr.open("GET", url, true);
 xhr.send();
 
@@ -26,16 +19,19 @@ let div = document.createElement('div');
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       div.innerHTML = this.responseText;
-      let scr = div.querySelector('#thumbnail');
       let thumbs = div.getElementsByTagName('img');
-      let vid_id = thumbs[0].src.replace("https://i.ytimg.com/vi",'');
-      vid_id = vid_id.match('^\/(.*?)\/')[1];
+      let vid_id = thumbs[0].src.replace("https://i.ytimg.com/vi",'').match('^\/(.*?)\/')[1];
       let final_url = "https://www.youtube.com/watch?v=" + vid_id;
 
+      // Get some stuff ready for the links
+      let hover = `onmouseover="this.style.textDecoration='underline'" onmouseout="this.style.textDecoration='none'"`;
+      let img_style = 'width="25" height="25" border="0" align="center"';
+      let primary_style = 'style="color: #c0392b"'
+      let secundary_style = 'style="margin-left: 20px; font-size: 8px; color: gray"'
+
       // Make the link appear
-      document.querySelectorAll("div._39LlV")[0].innerHTML += '<a style="color: #c0392b; text-decoration:none" href="' + final_url + '" target="_blank"><img src="' + chrome.extension.getURL("icon128.png") + '" width="25" height="25" border="0" align="center"  src=""/>   Watch on YouTube</a>';
-      // Also display the list if needed.
-      document.querySelectorAll("div._39LlV")[0].innerHTML += `<a style="margin-left: 20px; font-size: 8px; color: gray;" href="${url}" target="_blank" onmouseover="this.style.textDecoration='underline';"
-    onmouseout="this.style.textDecoration='none';">(or go to relevant search page)</a>`
+      document.querySelectorAll("div._39LlV")[0].innerHTML += `<a ${primary_style} href="${final_url}" target="_blank"><img ${img_style} src="${chrome.extension.getURL("icon128.png")}" />   Watch on YouTube</a>`;
+      // Also display the search results
+      document.querySelectorAll("div._39LlV")[0].innerHTML += `<a  ${secundary_style} ${hover} href="${url}" target="_blank">(or go to relevant search page)</a>`
     }
 };
